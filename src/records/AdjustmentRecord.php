b129a33663ae06f512a73465f4396e8752923898
Todd Kersey <todd@milesherndon.com>
2019-03-27 16:04:35 -0400

    Add DB install migration and AdjustmentRecord

---
+++ src/records/AdjustmentRecord.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Commerce Reports plugin for Craft CMS 3.x
+ *
+ * Plugin to run specific Commerce reports
+ *
+ * @link      https://milesherndon.com
+ * @copyright Copyright (c) 2019 MilesHerndon
+ */
+
+namespace milesherndon\commercereports\records;
+
+use milesherndon\commercereports\CommerceReports;
+
+use Craft;
+use craft\db\ActiveRecord;
+use yii\db\ActiveQueryInterface;
+
+/**
+ * @author    MilesHerndon
+ * @package   CommerceReports
+ * @since     1.0.5
+ */
+class AdjustmentRecord extends ActiveRecord
+{
+    // Public Static Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%commercereports_quantityversions}}';
+    }
+}
